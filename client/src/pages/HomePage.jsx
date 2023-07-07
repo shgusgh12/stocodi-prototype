9ebcb5da6c61d529d980038e96c3e38f91723edd
toothlessdev <toothless042@gmail.com>
2023-07-08 00:53:40 +0900

    design (HomePage) : ContentSection 의 ContentItemGroup 컴포넌트 생성 viewport 변경에 따라 그룹별로 flex-wrap 되도록 수정

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { HeroSection, RecommendSection, ContentSection, ContentItem } from "../components/HomeComponents";
+import { HeroSection, RecommendSection, ContentSection, ContentItem, ContentItemGroup } from "../components/HomeComponents";
 
 import thumbnail1 from "../assets/ContentItem1.png";
 import thumbnail2 from "../assets/ContentItem2.png";
@@ -15,10 +15,15 @@ export default function HomePage() {
             <RecommendSection></RecommendSection>
 
             <ContentSection title={"교육 컨텐츠"}>
-                <ContentItem title={"금융사기 피해예방"} instructor={"강사명"} time={"15:00"} thumbnail={thumbnail1}></ContentItem>
-                <ContentItem title={"똑소리 나게 돈 관리하기"} instructor={"강사명"} time={"14:00"} thumbnail={thumbnail2}></ContentItem>
-                <ContentItem title={"현명하게 돈 쓰기"} instructor={"강사명"} time={"13:29"} thumbnail={thumbnail3}></ContentItem>
-                <ContentItem title={"알차게 돈 모으기"} instructor={"강사명"} time={"15:84"} thumbnail={thumbnail4}></ContentItem>
+                <ContentItemGroup>
+                    <ContentItem title={"금융사기 피해예방"} instructor={"강사명"} time={"15:00"} thumbnail={thumbnail1}></ContentItem>
+                    <ContentItem title={"똑소리 나게 돈 관리하기"} instructor={"강사명"} time={"14:00"} thumbnail={thumbnail2}></ContentItem>
+                </ContentItemGroup>
+
+                <ContentItemGroup>
+                    <ContentItem title={"현명하게 돈 쓰기"} instructor={"강사명"} time={"13:29"} thumbnail={thumbnail3}></ContentItem>
+                    <ContentItem title={"알차게 돈 모으기"} instructor={"강사명"} time={"15:84"} thumbnail={thumbnail4}></ContentItem>
+                </ContentItemGroup>
             </ContentSection>
         </>
     );
